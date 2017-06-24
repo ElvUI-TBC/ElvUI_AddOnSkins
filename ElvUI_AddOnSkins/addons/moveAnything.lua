92ff744178a35f32f1093b9ec88374a977b44241
Crum <stschleyer89@gmail.com>
2017-06-23 21:05:23 -0500

    update MoveAnything skin

--- ElvUI_AddOnSkins/addons/moveAnything.lua
+++ ElvUI_AddOnSkins/addons/moveAnything.lua
@@ -6,33 +6,33 @@ local function LoadSkin()
 
 	local SPACING = 1 + (E.Spacing*2);
 
-	for i = 1, 20 do
-		_G["MAMover" .. i .. "Backdrop"]:SetTemplate("Transparent", nil, nil, true);
-		_G["MAMover" .. i]:HookScript("OnShow", function(self)
-			_G[self:GetName() .. "Backdrop"]:SetBackdropBorderColor(unpack(E["media"].rgbvaluecolor));
-		end);
-		_G["MAMover" .. i]:SetScript("OnEnter", function(self)
-			_G[self:GetName() .. "BackdropMovingFrameName"]:SetTextColor(1, 1, 1);
-		end);
-		_G["MAMover" .. i]:SetScript("OnLeave", function(self)
-			_G[self:GetName() .. "BackdropMovingFrameName"]:SetTextColor(unpack(E["media"].rgbvaluecolor));
-		end);
-	end
+	-- for i = 1, 20 do
+	-- 	_G["ResizingNudger" .. "Backdrop"]:SetTemplate("Transparent", nil, nil, true);
+	-- 	_G["ResizingNudger"]:HookScript("OnShow", function(self)
+	-- 		_G[self:GetName() .. "Backdrop"]:SetBackdropBorderColor(unpack(E["media"].rgbvaluecolor));
+	-- 	end);
+	-- 	_G["ResizingNudger"]:SetScript("OnEnter", function(self)
+	-- 		_G[self:GetName() .. "BackdropMovingFrameName"]:SetTextColor(1, 1, 1);
+	-- 	end);
+	-- 	_G["ResizingNudger"]:SetScript("OnLeave", function(self)
+	-- 		_G[self:GetName() .. "BackdropMovingFrameName"]:SetTextColor(unpack(E["media"].rgbvaluecolor));
+	-- 	end);
+	-- end
 
 	MAOptions:StripTextures();
 	MAOptions:SetTemplate("Transparent");
 	MAOptions:Size(420, 500 + (16*SPACING));
 
 	S:HandleCheckBox(MAOptionsCharacterSpecific);
-	S:HandleCheckBox(MAOptionsToggleTooltips);
-	S:HandleCheckBox(MAOptionsToggleModifiedFramesOnly);
-	S:HandleCheckBox(MAOptionsToggleCategories);
+	-- S:HandleCheckBox(MAOptionsToggleTooltips);
+	-- S:HandleCheckBox(MAOptionsToggleModifiedFramesOnly);
+	-- S:HandleCheckBox(MAOptionsToggleCategories);
 
 	S:HandleButton(MAOptionsResetAll);
 	S:HandleButton(MAOptionsClose);
-	S:HandleButton(MAOptionsSync);
+	-- S:HandleButton(MAOptionsSync);
 
-	for i = 1, 17 do
+	for i = 1, 10 do
 		_G["MAMove" .. i .. "Backdrop"]:SetTemplate("Default");
 		S:HandleCheckBox(_G["MAMove" .. i .. "Move"]);
 		S:HandleCheckBox(_G["MAMove" .. i .. "Hide"]);
@@ -43,27 +43,25 @@ local function LoadSkin()
 		end
 	end
 
-	MAScrollFrame:Size(380, 442 + (16*SPACING));
+	MAScrollFrame:Size(380, 232 + (16*SPACING));
 	S:HandleScrollBar(MAScrollFrameScrollBar);
 	MAScrollBorder:StripTextures();
 
-	MANudger:SetTemplate("Transparent");
-	S:HandleButton(MANudger_NudgeUp);
-	MANudger_NudgeUp:Point("CENTER", 0, 24 + SPACING);
-	S:HandleButton(MANudger_CenterMe);
-	MANudger_CenterMe:Point("TOP", MANudger_NudgeUp, "BOTTOM", 0, -SPACING);
-	S:HandleButton(MANudger_NudgeDown);
-	MANudger_NudgeDown:Point("TOP", MANudger_CenterMe, "BOTTOM", 0, -SPACING);
-	S:HandleButton(MANudger_NudgeLeft);
-	MANudger_NudgeLeft:Point("RIGHT", MANudger_CenterMe, "LEFT", -SPACING, 0);
-	S:HandleButton(MANudger_NudgeRight);
-	MANudger_NudgeRight:Point("LEFT", MANudger_CenterMe, "RIGHT", SPACING, 0);
-	S:HandleButton(MANudger_CenterH);
-	S:HandleButton(MANudger_CenterV);
-	S:HandleButton(MANudger_Detach);
-	S:HandleButton(MANudger_Hide);
-	S:HandleButton(MANudger_MoverPlus);
-	S:HandleButton(MANudger_MoverMinus);
+	ResizingNudger:SetTemplate("Transparent");
+	S:HandleButton(ResizingNudger_NudgeUp);
+	ResizingNudger_NudgeUp:Point("CENTER", 0, 24 + SPACING);
+	S:HandleButton(ResizingNudger_CenterMe);
+	ResizingNudger_CenterMe:Point("TOP", ResizingNudger_NudgeUp, "BOTTOM", 0, -SPACING);
+	S:HandleButton(ResizingNudger_NudgeDown);
+	ResizingNudger_NudgeDown:Point("TOP", ResizingNudger_CenterMe, "BOTTOM", 0, -SPACING);
+	S:HandleButton(ResizingNudger_NudgeLeft);
+	ResizingNudger_NudgeLeft:Point("RIGHT", ResizingNudger_CenterMe, "LEFT", -SPACING, 0);
+	S:HandleButton(ResizingNudger_NudgeRight);
+	ResizingNudger_NudgeRight:Point("LEFT", ResizingNudger_CenterMe, "RIGHT", SPACING, 0);
+	S:HandleButton(ResizingNudger_CenterH);
+	S:HandleButton(ResizingNudger_CenterV);
+	S:HandleButton(ResizingNudger_MoverPlus);
+	S:HandleButton(ResizingNudger_MoverMinus);
 
 	S:HandleButton(GameMenuButtonMoveAnything);
 end
