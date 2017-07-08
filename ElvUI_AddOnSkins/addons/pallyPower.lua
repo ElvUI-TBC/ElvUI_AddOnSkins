80a488b728242abc5f0c34d493a29b5b48967607
Pinya <800pin.ru@gmail.com>
2017-07-08 13:43:27 +0300

    update PallyPower skin, close ElvUI-TBC/ElvUI#230

--- ElvUI_AddOnSkins/addons/pallyPower.lua
+++ ElvUI_AddOnSkins/addons/pallyPower.lua
@@ -1,34 +1,33 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
 local unpack = unpack
 
--- PallyPower 3.2.20
+-- PallyPower 2.01.00
 
 local function LoadSkin()
-	if(not E.private.addOnSkins.PallyPower) then return; end
+	if not E.private.addOnSkins.PallyPower then return end
 
 	PallyPower.BuffScale = E.noop
 	PallyPower.ConfigScale = E.noop
-	PallyPower.ApplySkin = E.noop
 
 	PallyPower.opt.configscale = 1
 	PallyPower.opt.buffscale = 1
+
+	PallyPower.options.args.display.args.gapping.min = -1
 
 	local backdrop = E["media"].backdropfadecolor
 	PallyPower.db.profile.cBuffGood = {r = backdrop[1], g = backdrop[2], b = backdrop[3], t = backdrop[4]}
 	PallyPower.db.profile.cBuffNeedAll = {r = 0.5, g = 0.5, b = 0.5, t = backdrop[4]}
 	PallyPower.db.profile.cBuffNeedSome = {r = 0.5, g = 0.5, b = 0.5, t = backdrop[4]}
 	PallyPower.db.profile.cBuffNeedSpecial = {r = 0.5, g = 0.5, b = 0.5, t = backdrop[4]}
+	PallyPower.db.profile.display.gapping = -1
 
 	PallyPowerAuto:SetTemplate("Transparent", nil, true)
 	PallyPowerRF:SetTemplate("Transparent", nil, true)
-	PallyPowerAura:SetTemplate("Transparent", nil, true)
 
 	PallyPowerAutoIcon:SetTexCoord(unpack(E.TexCoords))
 	PallyPowerRFIcon:SetTexCoord(unpack(E.TexCoords))
-	PallyPowerRFIconSeal:SetTexCoord(unpack(E.TexCoords))
-	PallyPowerAuraIcon:SetTexCoord(unpack(E.TexCoords))
 
 	for i = 1, PALLYPOWER_MAXCLASSES do
 		local button = PallyPower.classButtons[i]
@@ -47,17 +46,10 @@ local function LoadSkin()
 	S:HandleCheckBox(PallyPowerConfigFrameFreeAssign)
 
 	S:HandleButton(PallyPowerConfigFrameOptions)
-	S:HandleButton(PallyPowerConfigFrameAutoAssign)
 	S:HandleButton(PallyPowerConfigFrameClear)
 	S:HandleButton(PallyPowerConfigFrameRefresh)
 
-	PallyPowerConfigFrameAuraGroup1AuraHeaderIcon:SetTexCoord(unpack(E.TexCoords))
-
-	for i = 1, 8 do
-		_G["PallyPowerConfigFramePlayer"..i.."Aura1Icon"]:SetTexCoord(unpack(E.TexCoords))
-	end
-
-	for i = 1, 11 do
+	for i = 1, PALLYPOWER_MAXCLASSES do
 		_G["PallyPowerConfigFrameClassGroup"..i.."ClassButtonIcon"]:SetTexCoord(unpack(E.TexCoords))
 
 		for j = 1, 8 do
@@ -69,4 +61,4 @@ local function LoadSkin()
 	E:GetModule("AddOnSkins"):SkinLibrary("Dewdrop-2.0")
 end
 
-S:AddCallbackForAddon("PallyPower", "PallyPower", LoadSkin);
+S:AddCallbackForAddon("PallyPower", "PallyPower", LoadSkin)
