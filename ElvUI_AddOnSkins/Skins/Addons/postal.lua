d84649acd68cd15900f888466ccb1b1954e808d1
fatlus <julian.doss@gmail.com>
2018-11-30 21:32:25 +0100

    Return Postal, Talented and ZOMGBuffs Skins
    - Added Talented and ZOMGBuffs Skins
    - Fixed Postal Skin

--- ElvUI_AddOnSkins/Skins/Addons/postal.lua
+++ ElvUI_AddOnSkins/Skins/Addons/postal.lua
@@ -1,69 +1,46 @@
-local E, L, V, P, G = unpack(ElvUI)
-local S = E:GetModule("Skins")
-
--- Postal 2.1
-
-local _G = _G
-local INBOXITEMS_TO_DISPLAY = INBOXITEMS_TO_DISPLAY
+local E, L, V, P, G, _ = unpack(ElvUI);
+local S = E:GetModule("Skins");
 
 local function LoadSkin()
-	if not E.private.addOnSkins.Postal then return end
+	if(not E.private.addOnSkins.Postal) then return; end
 
-	S:HandleNextPrevButton(Postal_ModuleMenuButton, true)
-	Postal_ModuleMenuButton:SetPoint("TOPRIGHT", MailFrame, -60, -16)
-
-	hooksecurefunc(Postal_Select, "OnEnable", function()
-		if PostalSelectOpenButton and not PostalSelectOpenButton.isSkinned then
-			S:HandleButton(PostalSelectOpenButton, true)
-			PostalSelectOpenButton:Point("RIGHT", InboxFrame, "TOP", -22, -58)
-
-			PostalSelectOpenButton.isSkinned = true
-		end
-
-		if PostalSelectReturnButton and not PostalSelectReturnButton.isSkinned then
-			S:HandleButton(PostalSelectReturnButton, true)
-			PostalSelectReturnButton:Point("LEFT", InboxFrame, "TOP", 13, -58)
-
-			PostalSelectReturnButton.isSkinned = true
-		end
-
-		for i = 1, INBOXITEMS_TO_DISPLAY do
-			local item = _G["MailItem"..i.."ExpireTime"]
-			local checkbox = _G["PostalInboxCB"..i]
-
-			if item and not item.isSkinned then
-				item:SetPoint("TOPRIGHT", "MailItem"..i, "TOPRIGHT", -5, -10)
-				if item.returnicon then
-					item.returnicon:Point("TOPRIGHT", item, "TOPRIGHT", 20, 0)
-				end
-
-				item.isSkinned = true
-			end
-
-			if checkbox and not checkbox.isSkinned then
-				S:HandleCheckBox(_G["PostalInboxCB"..i])
-
-				checkbox.isSkinned = true
+	for i = 1, INBOXITEMS_TO_DISPLAY do
+		local item = _G["MailItem" .. i .. "ExpireTime"];
+		if(item) then
+			item:SetPoint("TOPRIGHT", "MailItem" .. i, "TOPRIGHT", -5, -10);
+			if(item.returnicon) then
+				item.returnicon:Point("TOPRIGHT", item, "TOPRIGHT", 20, 0);
 			end
 		end
-	end)
-
-	hooksecurefunc(Postal_OpenAll, "OnEnable", function()
-		if PostalOpenAllButton and not PostalOpenAllButton.isSkinned then
-			S:HandleButton(PostalOpenAllButton, true)
-			PostalOpenAllButton:Point("CENTER", InboxFrame, "TOP", -17, -410)
-
-			PostalOpenAllButton.isSkinned = true
+		if(_G["PostalInboxCB" .. i]) then
+			S:HandleCheckBox(_G["PostalInboxCB" .. i]);
 		end
+	end
 
-		if Postal_OpenAllMenuButton and not Postal_OpenAllMenuButton.isSkinned then
-			S:HandleNextPrevButton(Postal_OpenAllMenuButton, true)
-			Postal_OpenAllMenuButton:Point("LEFT", PostalOpenAllButton, "RIGHT", 2, 0)
-			Postal_OpenAllMenuButton:Height(25)
-			
-			Postal_OpenAllMenuButton.isSkinned = true
-		end
-	end)
+	if PostalSelectOpenButton then
+		S:HandleButton(PostalSelectOpenButton, true);
+	end
+
+	if Postal_OpenAllMenuButton then
+		S:HandleNextPrevButton(Postal_OpenAllMenuButton, true);
+	end
+
+	if PostalOpenAllButton then
+		S:HandleButton(PostalOpenAllButton, true);
+	end
+
+	if PostalSelectReturnButton then
+		S:HandleButton(PostalSelectReturnButton, true);
+	end
+
+	if Postal_ModuleMenuButton then
+		S:HandleNextPrevButton(Postal_ModuleMenuButton, true);
+		Postal_ModuleMenuButton:SetPoint("TOPRIGHT", MailFrame, -60, -16);
+	end
+
+	if Postal_BlackBookButton then
+		S:HandleNextPrevButton(Postal_BlackBookButton, true);
+	end
 
 	hooksecurefunc(Postal_BlackBook, "OnEnable", function()
 		if Postal_BlackBookButton and not Postal_BlackBookButton.isSkinned then
@@ -73,14 +50,6 @@ local function LoadSkin()
 			Postal_BlackBookButton.isSkinned = true
 		end
 	end)
-
-	InboxPrevPageButton:Point("CENTER", InboxFrame, "BOTTOMLEFT", 62, 104)
-	InboxPrevPageButton.SetPoint = E.noop
-
-	InboxNextPageButton:Point("CENTER", InboxFrame, "BOTTOMLEFT", 313, 104)
-	InboxNextPageButton.SetPoint = E.noop
-
-	E:GetModule("AddOnSkins"):SkinLibrary("Dewdrop-2.0")
 end
 
-S:AddCallbackForAddon("Postal", "Postal", LoadSkin)
+S:AddCallbackForAddon("Postal", "Postal", LoadSkin);
